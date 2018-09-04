7dc3f11730fee47e0556e09e0ccd458522b27aae
Lorenzo <sheepytaggernep@gmail.com>
2018-09-04 15:53:07 +0200

    Quaternion test

---
+++ Cool Game/Assets/Tests/NewTestScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class NewTestScript {
+
+    [Test]
+    public void NewTestScriptSimplePasses() {
+        // Use the Assert class to test conditions.
+    }
+
+    // A UnityTest behaves like a coroutine in PlayMode
+    // and allows you to yield null to skip a frame in EditMode
+    [UnityTest]
+    public IEnumerator NewTestScriptWithEnumeratorPasses() {
+        // Use the Assert class to test conditions.
+        // yield to skip a frame
+        yield return null;
+    }
+
+    [Test]
+    public void TestQuaternion()
+    {
+        Vector3 expected = new Vector3(0,10,0);
+        Quaternion q = Quaternion.Euler(expected);
+        
+        Assert.IsTrue(expected.Equals(q.eulerAngles));
+    }
+}
