33b654249259428a20eee17acf575db5751fb5e8
Lorenzo <sheepytaggernep@gmail.com>
2018-10-30 11:54:20 +0100

    fixed music + started with tests

---
+++ Cool Game/Assets/Tests/CombatManagerTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using NUnit.Framework;
+using Unit;
+
+namespace Tests
+{
+    public class CombatManagerTests
+    {
+        private const int _DEFAULT_START_HP = 100;
+        private const int _DEFAULT_DMG = 10;
+
+        [Test]
+        public void Can_Create()
+        {
+            CombatManager cm = new CombatManager(new Creature(String.Empty, _DEFAULT_START_HP, _DEFAULT_START_HP),
+                new Creature(String.Empty, _DEFAULT_START_HP, _DEFAULT_START_HP));
+            Assert.IsNotNull(cm);
+        }
+    }
+}
---
+++ Cool Game/Assets/Tests/Unit/AbilityTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using NUnit.Framework;
+using Unit;
+
+namespace Tests.Unit
+{
+    public class AbilityTests
+    {
+        [Test]
+        public void Can_Create()
+        {
+            Ability a = new Ability(String.Empty, null);
+            Assert.IsNotNull(a);
+        }
+    }
+}
