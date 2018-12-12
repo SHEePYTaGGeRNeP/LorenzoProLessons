f054247caa3eb79009f453ff93a01581ff817b3f
Lorenzo <sheepytaggernep@gmail.com>
2018-12-12 12:26:43 +0100

    Added Interface/Inheritance tests

--- Cool Game/Assets/Tests/NewTestScript.cs
+++ Cool Game/Assets/Tests/NewTestScript.cs
@@ -1,9 +1,14 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.TestTools;
 using NUnit.Framework;
 using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
 using Jobs;
 using UnityEngine.Jobs;
+using UnityEngine.Windows;
 
 namespace Tests
 {
@@ -14,7 +19,7 @@ namespace Tests
         {
             // Use the Assert class to test conditions.
             Assert.IsTrue(true);
-            Assert.AreEqual(1,1);
+            Assert.AreEqual(1, 1);
         }
 
         // A UnityTest behaves like a coroutine in PlayMode
@@ -38,6 +43,75 @@ namespace Tests
             //Debug.Log($"q {q.eulerAngles}");
             //Debug.Log($"e {expected}");
             Assert.AreEqual(expected, q.eulerAngles);
+            List<object> ls;
+        }
+
+
+        [Test]
+        public void Testttt()
+        {
+            List<Animal> animals = new List<Animal>
+            {
+                new Beetle(),
+                new Shark(),
+                new Duck()
+            };
+            foreach (Animal a in animals)
+                a.Eat();
+            List<ISwimmable> swimmables = new List<ISwimmable>()
+            {
+                new Duck(),
+                new Shark()
+            };
+            foreach (ISwimmable a in swimmables)
+                a.Swim();
+        }
+
+
+        private abstract class Animal
+        {
+            public void Eat()
+            {
+                LogHelper.Log(typeof(Animal),"Nomnomnom");
+            }
+        }
+
+        private interface ISwimmable
+        {
+            int SwimSpeed { get; set; }
+            void Swim();
+        }
+
+        private interface IFlyable
+        {
+            void Fly();
+        }
+
+        private class Beetle : Animal
+        {
+        }
+
+        private class Duck : Animal, ISwimmable, IFlyable
+        {
+            public int SwimSpeed { get; set; }
+
+            public void Swim()
+            {
+            }
+
+            public void Fly()
+            {
+                
+            }
+        }
+
+        private class Shark : Animal, ISwimmable
+        {
+            public int SwimSpeed { get; set; }
+
+            public void Swim()
+            {
+            }
         }
     }
 }
