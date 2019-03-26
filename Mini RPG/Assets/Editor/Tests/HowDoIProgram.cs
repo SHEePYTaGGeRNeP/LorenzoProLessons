a9054ec552dd38c76a5946102e8fa2718454a944
Lorenzo <sheepytaggernep@gmail.com>
2019-03-26 15:39:47 +0100

    Actions_vs_Events_MultipleSubscribe test

--- Mini RPG/Assets/Editor/Tests/HowDoIProgram.cs
+++ Mini RPG/Assets/Editor/Tests/HowDoIProgram.cs
@@ -17,6 +17,48 @@ namespace Assets.Editor.Tests
             Assert.IsFalse(i.Any(x => x > 5));
             Assert.IsTrue(i.All(x => x > 5));
         }
+
+        /// <summary>I thought event would only allow you to subscribe once with the same method</summary>
+        [Test]
+        public void Actions_vs_Events_MultipleSubscribe()
+        {
+            EventClass ec = new EventClass();
+            int count = 0;
+            void TestAction()
+            {
+                count++;
+            }
+            ec.SubscribeMeAction += TestAction;
+            ec.SubscribeMeAction += TestAction;
+            ec.SubscribeMeAction += TestAction;
+            ec.SubscribeMeAction.Invoke();
+            Assert.AreEqual(3, count);
+            count = 0;
+            ec.SubscribeMeActionEvent += TestAction;
+            ec.SubscribeMeActionEvent += TestAction;
+            ec.SubscribeMeActionEvent += TestAction;
+            ec.Invoke();
+            Assert.AreEqual(3, count);
+            count = 0;
+            void TestHandler(object sender, EventArgs e)
+            {
+                count++;
+            }
+            ec.SubscribeMeEvent += TestHandler;
+            ec.SubscribeMeEvent += TestHandler;
+            ec.SubscribeMeEvent += TestHandler;
+            ec.InvokeEvent();
+            Assert.AreEqual(3, count);
+        }
+        private class EventClass
+        {
+            public event Action SubscribeMeActionEvent;
+            public event EventHandler SubscribeMeEvent;
+            public Action SubscribeMeAction;
+            public void Invoke() => this.SubscribeMeActionEvent.Invoke();
+            public void InvokeEvent() => this.SubscribeMeEvent.Invoke(this, EventArgs.Empty);
+
+        }
         
     }
 }
