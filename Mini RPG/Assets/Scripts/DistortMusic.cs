788414a9846fda6de11ff0d6f783a28d2151a2c7
Lorenzo <neppy@live.nl>
2019-04-28 15:53:42 +0200

    Added music + mixer effect based on % HP

---
+++ Mini RPG/Assets/Scripts/DistortMusic.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Units;
+using UnityEngine;
+using UnityEngine.Audio;
+
+namespace Assets.Scripts
+{
+
+    public class DistortMusic : MonoBehaviour
+    {
+        [SerializeField]
+        private AudioMixer _audioMixer;
+
+        [SerializeField]
+        private AnimationCurve _distortionCurve;
+        [SerializeField]
+        private AnimationCurve _pitchCurve;
+
+        private const string _DISTORTION_NAME = "MusicDistortion";
+        private const string _PITCH_NAME = "MusicPitch";
+
+
+
+        public void OnHealthUpdate(HealthChangeEventArgs e)
+        {
+            float perc = Mathf.Clamp01(1 - (float)e.CurrentHitPoints / e.MaxHitPoints);
+            this._audioMixer.SetFloat(_DISTORTION_NAME, _distortionCurve.Evaluate(perc));
+            this._audioMixer.SetFloat(_PITCH_NAME, _pitchCurve.Evaluate(perc));
+        }
+    }
+}
