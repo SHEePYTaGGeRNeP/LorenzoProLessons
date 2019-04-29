b4a4aec0e66d13f1826d5f482d0b0365a9756bda
Lorenzo <lorenzo.suiker@dutchworkz.nl>
2019-04-29 11:25:40 +0200

    Added Pause with ESC

---
+++ Mini RPG/Assets/Scripts/Pause.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+public class Pause : MonoBehaviour
+{
+    [SerializeField]
+    private AudioMixerSnapshot _unpausedSnapshot;
+    [SerializeField]
+    private AudioMixerSnapshot _pausedSnapshot;
+
+    private bool _isPaused = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!Input.GetKeyUp(KeyCode.Escape))
+            return;
+        this._isPaused = !this._isPaused;
+        AudioMixerSnapshot snapshotTo = this._isPaused ? this._pausedSnapshot : this._unpausedSnapshot;
+        snapshotTo.TransitionTo(0f);
+        Time.timeScale = this._isPaused ? 0 : 1;
+    }
+
+
+}
